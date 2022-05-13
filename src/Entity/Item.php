4d60331b72e16eaa6626f4935df3e35261c959fd
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-05-13 12:05:49 +0200

    item entities

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -16,6 +16,12 @@ class Item
     #[ORM\Column(type: 'boolean')]
     private $isAvailable;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'float')]
+    private $price;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +35,30 @@ class Item
     public function setIsAvailable(bool $isAvailable): self
     {
         $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
 
         return $this;
     }
