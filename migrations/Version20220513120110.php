5a48033dbad77bfcb6b5f6963fd8adae5f1d04d7
GRILLO-Benjamin <benjisensei@gmail.com>
2022-05-13 14:02:59 +0200

    l

--- migrations/Version20220513120110.php
+++ migrations/Version20220513120110.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220513100418 extends AbstractMigration
+final class Version20220513120110 extends AbstractMigration
 {
     public function getDescription(): string
     {
